fcee361c51048c0f2b631ba6a30d04ccd2052847
unknown <Administrator@.(none)>
2010-02-27 00:23:20 -0800

    add spec

--- aCache/pluginMain.cpp
+++ aCache/pluginMain.cpp
@@ -28,7 +28,7 @@
 MStatus initializePlugin( MObject obj ) 
 {
 MStatus status;
-MFnPlugin plugin( obj, "ZHANG JIAN", "build 4.2.3 Mon Feb 22 2010", "Any" );
+MFnPlugin plugin( obj, "ZHANG JIAN", "build 4.3.0 Fri Feb 26 2010", "Any" );
 
 /*
 	
